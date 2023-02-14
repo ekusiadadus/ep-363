32a8946ddf3d49473441ec8809b3b3caa005d591
ekusiadadus <ekusiadadus@gmail.com>
2023-02-14 12:34:34 +0900

    feat: :fire: add unions constructor and deconstructor

--- main.cpp
+++ main.cpp
@@ -6,21 +6,31 @@ using namespace std;
 // only one member is active(alive) at a time
 // you can only access the currently active member
 // * any other access is undefined behavior
+// No member is the default member
+// Unions can have constructors and destrucotrs in C++11
 
 consteval auto use_union() {
     union U {
-        int i;
-        float f;
-        double d;
+//        int i;
+//        float f;
+//        double d;
+        U() {}
+        ~U(){
+            s.std::string::~string();
+        }
+
+        std::string s;
     };
 
     U u;
+    u.s = std::string();
+    std::construct_at(&u.s, "Hello, World Long String!");
 
     // size of the union is the largest element of itself
-    return sizeof(U);
+//    return sizeof(U);
 //    u.f = 4.2;
 //    u.i = 42;
-//    return u.i;
+    return u.s.size();
 }
 
 // union can be anonymous

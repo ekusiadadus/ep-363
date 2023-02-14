ef5cc5fe59e2d2bda4f120eb4ffb2cc02be83855
ekusiadadus <ekusiadadus@gmail.com>
2023-02-14 12:17:16 +0900

    feat: :union: may be unused

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+
+consteval auto use_union() {
+    union {
+        int i;
+        float f;
+    };
+    f = 42;
+    return i;
+}
+
+// union can be anonymous
+int main() {
+   [[maybe_unused]] constexpr auto v = use_union();
+}

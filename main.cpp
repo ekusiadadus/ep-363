7c2b0b94ec1368637c261f986d732b75b5f45f2e
ekusiadadus <ekusiadadus@gmail.com>
2023-02-14 12:22:53 +0900

    feat: :fire: size of union

--- main.cpp
+++ main.cpp
@@ -4,15 +4,23 @@ using namespace std;
 
 
 consteval auto use_union() {
-    union {
+    union U {
         int i;
         float f;
     };
-    i = 42;
-    return i;
+
+    U u;
+
+    // size of the union is the largest element of itself
+    return sizeof(U);
+//    u.f = 4.2;
+//    u.i = 42;
+//    return u.i;
 }
 
 // union can be anonymous
 int main() {
    [[maybe_unused]] constexpr auto v = use_union();
+   std::cout << v << std::endl;
+   return v;
 }
